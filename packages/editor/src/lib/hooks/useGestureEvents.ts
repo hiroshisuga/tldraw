9c79af32574b3b7cb3ab237ca57455e70d1c0250
hiroshisuga <45039819+hiroshisuga@users.noreply.github.com>
2026-08-22 21:40:48 +0900

    Adjust pinch event coordinates to container bounds

--- packages/editor/src/lib/hooks/useGestureEvents.ts
+++ packages/editor/src/lib/hooks/useGestureEvents.ts
@@ -146,6 +146,7 @@ export function useGestureEvents(ref: React.RefObject<HTMLDivElement>) {
 			pinchState = 'not sure'
 
 			const { event, origin, da } = gesture
+			const container = editor.getContainer().getBoundingClientRect()
 
 			if (event instanceof WheelEvent) return
 			if (!(event.target === elm || elm?.contains(event.target as Node))) return
@@ -160,7 +161,7 @@ export function useGestureEvents(ref: React.RefObject<HTMLDivElement>) {
 			editor.dispatch({
 				type: 'pinch',
 				name: 'pinch_start',
-				point: { x: origin[0], y: origin[1], z: editor.getZoomLevel() },
+				point: { x: origin[0] - container.left, y: origin[1] - container.top, z: editor.getZoomLevel() },
 				delta: { x: 0, y: 0 },
 				shiftKey: event.shiftKey,
 				altKey: event.altKey,
@@ -190,7 +191,7 @@ export function useGestureEvents(ref: React.RefObject<HTMLDivElement>) {
 
 			switch (pinchState) {
 				case 'not sure': {
-					if (touchDistance > 24) {
+					if (touchDistance > 8) {
 						pinchState = 'zooming'
 					} else if (originDistance > 16) {
 						pinchState = 'panning'
@@ -230,6 +231,7 @@ export function useGestureEvents(ref: React.RefObject<HTMLDivElement>) {
 
 			const dx = origin[0] - prevPointBetweenFingers.x
 			const dy = origin[1] - prevPointBetweenFingers.y
+			const container = editor.getContainer().getBoundingClientRect()
 
 			prevPointBetweenFingers.x = origin[0]
 			prevPointBetweenFingers.y = origin[1]
@@ -243,7 +245,7 @@ export function useGestureEvents(ref: React.RefObject<HTMLDivElement>) {
 					editor.dispatch({
 						type: 'pinch',
 						name: 'pinch',
-						point: { x: origin[0], y: origin[1], z: currZoom },
+						point: { x: origin[0] - container.left, y: origin[1] - container.top, z: currZoom },
 						delta: { x: dx, y: dy },
 						shiftKey: event.shiftKey,
 						altKey: event.altKey,
@@ -274,6 +276,7 @@ export function useGestureEvents(ref: React.RefObject<HTMLDivElement>) {
 			if (!(event.target === elm || elm?.contains(event.target as Node))) return
 
 			const scale = offset[0]
+			const container = editor.getContainer().getBoundingClientRect()
 
 			pinchState = 'not sure'
 
@@ -281,8 +284,8 @@ export function useGestureEvents(ref: React.RefObject<HTMLDivElement>) {
 				editor.dispatch({
 					type: 'pinch',
 					name: 'pinch_end',
-					point: { x: origin[0], y: origin[1], z: scale },
-					delta: { x: origin[0], y: origin[1] },
+					point: { x: origin[0] - container.left, y: origin[1] - container.top, z: scale },
+					delta: { x: origin[0] - container.left, y: origin[1] - container.top },
 					shiftKey: event.shiftKey,
 					altKey: event.altKey,
 					ctrlKey: event.metaKey || event.ctrlKey,
